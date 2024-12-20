34bcaa15a7a95eae5aea85fb7dcd5d3d6b2d6ed8
tuyen.laivan <tuyen.laivan@vti.com.vn>
2024-12-20 10:39:34 +0700

    base

---
+++ src/main/java/com/vti/Spring/Boot/MpAshe/Base/SpringBootMpAsheBaseApplication.java
@@ -0,0 +1,13 @@
+package com.vti.Spring.Boot.MpAshe.Base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMpAsheBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMpAsheBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vti/Spring/Boot/MpAshe/Base/SpringBootMpAsheBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.Spring.Boot.MpAshe.Base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMpAsheBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
